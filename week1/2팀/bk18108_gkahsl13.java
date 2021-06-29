525391984e8578a053b15c26fee446fb7edafddd
oh-seungmin <79954717+oh-seungmin@users.noreply.github.com>
2021-06-29 16:07:50 +0900

    1주차 과제 2번 문제풀이

---
+++ week1/2팀/bk18108_gkahsl13.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bk18108_gkahsl13 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int year=in.nextInt();
+		year-=543;
+		
+		System.out.println(year);
+	}
+
+}
