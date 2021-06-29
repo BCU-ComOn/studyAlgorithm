0caa32b83e03aa91180e49ab947f22b8d7245641
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-06-29 16:31:03 +0900

    Create bj5532_jdh.java

---
+++ week1/4팀/bj5532_jdh.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class bj5532_jdh {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int L = in.nextInt();
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		int D = in.nextInt();	
+		int count = 0;
+		
+		while(true) {
+			A -= C;
+			B -= D;
+			count++;
+			if (A <= 0 && B <= 0) break;
+		}
+		System.out.println(L - count);
+	}
+
+}
