db9f6670a5365ad50b18d1f7c83b2aecd9d4f0b8
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-07-04 11:07:26 +0900

    1주차_15727

---
+++ week1/팀4/bj15727_jdh.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bj15727_jdh {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int L = in.nextInt();
+		int count = 0;
+		
+		while (true) {
+			L -= 5;
+			count += 1;
+			if (L <= 0) break;
+		}
+		System.out.println(count);
+	}
+
+}
