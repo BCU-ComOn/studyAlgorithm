92d86156436b005834f5c0e221e846a155a97875
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-07-04 11:11:57 +0900

    1주차_18108

---
+++ week1/팀4/bj18108_jdh.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bj18108_jdh {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int y = in.nextInt();
+		
+		System.out.println(y - 543);
+	}
+
+}
