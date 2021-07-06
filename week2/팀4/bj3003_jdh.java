5de2d81fef6eb3c2d4e72823ceaa5b1894e60fcb
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-07-06 15:32:26 +0900

    2주차_3003

---
+++ week2/팀4/bj3003_jdh.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class bj3003_jdh {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String bufferArr[] = in.readLine().split(" ");
+		int arr[] = new int[bufferArr.length];
+		for (int i = 0; i < bufferArr.length; i++) arr[i] = Integer.parseInt(bufferArr[i]);
+		
+		int temp[] = {1, 1, 2, 2, 2, 8};
+		
+		for (int i = 0; i < arr.length; i++) {
+			out.write(String.valueOf(temp[i] - arr[i]));
+			out.write(" ");
+		}
+		out.close();
+	}
+}
