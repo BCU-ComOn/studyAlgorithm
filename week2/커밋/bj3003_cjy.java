89b9c427531d4d8ef7af9b79dbf55f38dc02c0ee
choijiyeon20 <jiyeonchoi2020@gmail.com>
2021-07-06 21:39:10 +0900

    2주차 과제 풀이 최지연

---
+++ week2/커밋/bj3003_cjy.java
@@ -0,0 +1,19 @@
+package studyalgorithm;
+//킹, 퀸, 룩, 비숍, 나이트, 폰
+import java.util.Scanner;
+
+public class bj3003_cjy {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int K = s.nextInt() - 1;
+		int Q = s.nextInt() - 1;
+		int R = s.nextInt() - 2;
+		int B = s.nextInt() - 2;
+		int Kn = s.nextInt() - 2;
+		int P = s.nextInt() - 8;
+		System.out.println(K*-1+" "+Q*-1+" "+R*-1+" "
+				+B*-1+" "+Kn*-1+" "+P*-1);
+	}
+}
