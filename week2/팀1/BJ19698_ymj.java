835f53671892abf41eb22b7a55c27cc179d7a264
ASIANAKOREA <81851326+ASIANAKOREA@users.noreply.github.com>
2021-07-06 15:48:39 +0900

    2주차 과제 2번 팀1 정유민

---
+++ week2/팀1/BJ19698_ymj.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class BJ19698_ymj {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int[] num = new int[4];
+		
+		for(int i = 0; i<num.length; i++) {
+			num[i] = s.nextInt();
+		}
+		
+		int num2 = (num[1]/num[3]) * (num[2]/num[3]);
+		if(num[0]<num2) System.out.println(num[0]);
+		else System.out.println(num2);
+	}
+}
