caf1f26094172b9cb17619388f23df5d53804fd4
oh-seungmin <79954717+oh-seungmin@users.noreply.github.com>
2021-07-06 15:49:28 +0900

    2주차 과제 2번-오승민

---
+++ week2/팀2/bk19698_gkahsl13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class bk19698_gkahsl13 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a =in.nextInt();
+		int b =in.nextInt();
+		int c =in.nextInt();
+		int d =in.nextInt();
+		
+		b/=d;
+		c/=d;
+		
+		System.out.println(Math.min(b*c,a));
+	}
+}
