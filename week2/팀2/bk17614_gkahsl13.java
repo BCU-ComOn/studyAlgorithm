d753eea681374f5c18f4ec9bef9caf7835b990f1
oh-seungmin <79954717+oh-seungmin@users.noreply.github.com>
2021-07-06 15:10:55 +0900

    2주차 3번문제

---
+++ week2/팀2/bk17614_gkahsl13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class bk17614_gkahsl13 {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int a =in.nextInt();
+	int count=0;
+	for(int i=1;i<=a;i++) {
+		if(i%10==3||i%10==6||i%10==9)
+			count++;
+		if(i/10==3 ||i/10==6 || i/10==9)
+			count++;		
+	}
+	System.out.println(count);
+	}
+
+}
