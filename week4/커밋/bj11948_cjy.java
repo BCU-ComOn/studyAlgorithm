7770ab0472ad6e92a20aee1653f3a16131957514
choijiyeon20 <jiyeonchoi2020@gmail.com>
2021-07-27 15:11:34 +0900

    4주차 문제 풀이 최지연

---
+++ week4/커밋/bj11948_cjy.java
@@ -0,0 +1,39 @@
+// 과목선택
+
+import java.util.Scanner;
+
+public class bj11948_cjy {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int[] arraySci = new int[4];
+		int[] arraySoc = new int[2];
+		
+		int minSci = 100;
+		int minSoc = 100;
+		
+		int maxSci = 0;
+		int maxSoc = 0;
+		
+		for(int i=0; i<arraySci.length; i++) {
+			arraySci[i]= s.nextInt();
+			if(arraySci[i]<minSci)
+				minSci = arraySci[i];
+		}
+		for(int i=0; i<arraySoc.length; i++) {
+			arraySoc[i] = s.nextInt();
+			if(arraySoc[i]<minSoc)
+				minSoc = arraySoc[i];
+		}
+
+		
+		for(int i=0; i<arraySci.length; i++) {
+			maxSci +=arraySci[i];
+		}
+		for(int i=0; i<arraySoc.length; i++) {
+			 maxSoc += arraySoc[i];
+		}
+
+		System.out.println((maxSci-minSci)+(maxSoc-minSoc));
+	}
+}
