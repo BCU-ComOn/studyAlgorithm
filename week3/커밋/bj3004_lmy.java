75fa8740061c052857f9d30f9320338764dfe61e
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-08-02 02:33:55 +0900

    .

--- week3/커밋/bj3004_lmy.java
+++ week3/커밋/bj3004_lmy.java
@@ -3,12 +3,12 @@ package week3.커밋;
 import java.util.Scanner;
 
 public class bj3004_lmy {
-    public static void main(String[] args) 
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-		int a = N /2 +1;
-		int b = N  - a + 2;
-		int c = a*b;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int a = N / 2 + 1;
+		int b = N - a + 2;
+		int c = a * b;
 		System.out.println(c);
-}
+	}
 }
