9ae4af2309f545c0f4a696979767ff2c5ec529bb
hcb1999 <79966891+hcb1999@users.noreply.github.com>
2021-08-03 16:11:49 +0900

    문제3번 완료했습니다.

---
+++ week5/팀2/bj2455_hcb.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bj2455_hcb {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sta[][] = new int[4][2];
+		int num[] = new int[4];
+		int max=0;
+		for(int i=0; i<4; i++) {
+		   for(int j=0; j<2;j++) {
+			  sta[i][j] = sc.nextInt();
+			 
+    		}
+		   if(i==0) num[0]=sta[0][1];
+		   else num[i] = num[i-1]-sta[i][0]+sta[i][1]; 
+		   if(num[i]>max) max = num[i];
+		}
+		System.out.println(max);
+		
+	}
+
+}
