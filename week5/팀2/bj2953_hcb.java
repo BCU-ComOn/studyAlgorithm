6d80b002d39b82cc3d9fd705144c92e82174e551
hcb1999 <79966891+hcb1999@users.noreply.github.com>
2021-08-07 17:45:50 +0900

    과제3번완료했습니다.

---
+++ week5/팀2/bj2953_hcb.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class bj2953_hcb {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int num[][] =new int[5][4];
+        int sum[]= new int[5];
+        int i;
+        int s=0;
+        int max=0;
+    for(i=0; i<5; i++) {    
+        for(int j=0; j<4; j++) {
+        	num[i][j]=sc.nextInt(); 
+        	sum[i]+=num[i][j];
+        }
+    	
+    		   
+       }
+    for(int b=0;b<5;b++) {
+    	if(max<sum[b]) { 
+    		 max =sum[b];
+    	     s=b+1;
+    	}
+       	
+       	
+    }
+    
+    System.out.println(s+" "+max);
+   
+	}
+
+}
